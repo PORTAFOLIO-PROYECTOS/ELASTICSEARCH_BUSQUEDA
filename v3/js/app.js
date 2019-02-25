86610dfe33e8ba8e45f5bfd5266bd922826d3bb9
HugoRoca <hugo.rock20@hotmail.com>
2019-02-25 17:59:39 -0500

    more changes

---
+++ v3/js/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const app = (function() {
+
+    const _elementos = {
+        cargarPantalla: ".cargaPantalla",
+        errorMensaje: ".alert-danger",
+        opcionIndex: "#opcion-index",
+        opcionQuery: "#opcion-query",
+        
+    }
+
+})();
+
+$(document).ready(function(){
+
+});
