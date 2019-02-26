7fda9172aff2a752dc4feeb5b38a23c0a19e4887
Hugo <hugo.rock20@hotmail.com>
2019-02-26 00:13:52 -0500

    more chnages

--- v3/js/app.js
+++ v3/js/app.js
@@ -34,6 +34,44 @@ const app = (function () {
         },
         cargando: function (val) {
             return val ? $(_elementos.cargarPantalla).fadeIn("slow") : $(_elementos.cargarPantalla).fadeOut("slow");
+        },
+        ejecutarQuery: function (query) {
+            let indice = _funciones.obtenerIndice();
+
+            return $.ajax({
+                url: `${_config.urlES}/${indice}/_search`,
+                type: "post",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(query)
+            });
+        },
+        construirQuery: function () {
+
+        },
+        obtenerIndice: function () {
+            return $(_elementos.opcionIndex).find("option:selected").val();
+        },
+        obtenerMultiMatch: function (opcion) {
+            let query = null;
+            let textoBusqueda = $(_elementos.textoBusqueda).val();
+
+            switch (opcion) {
+                case "1":
+                    query = getQuery1(textoBusqueda)
+                    break;
+                case "2":
+                    query = getQuery2(textoBusqueda)
+                    break;
+                case "3":
+                    query = getQuery3(textoBusqueda)
+                    break;
+                case "4":
+                    query = getQuery4(textoBusqueda)
+                    break;
+            }
+
+            return query;
         }
     }
 
